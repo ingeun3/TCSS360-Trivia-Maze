2a56f062519d3e3586d57f9136952f370705ee2c
Khin Win <kwin2@uw.edu>
2023-05-09 08:52:29 -0700

    second time

--- triva_Maze/src/Main.java
+++ triva_Maze/src/Main.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Maze maze = new Maze();
+        Maze maze = new Maze("maze_map1.txt");
         maze.setArray(new Point(2,2));
         System.out.println(maze);
 
--- triva_Maze/src/Maze.java
+++ triva_Maze/src/Maze.java
@@ -7,17 +7,20 @@ import java.util.Scanner;
 
 public class Maze {
 
-    // the default map file for maze.
-    private static final File MAZE_MAP =  new File ("maze_map1.txt");
-
-    // the final Radom.
+    // the final Random.
     private static Random RANDOM_POINTS = new Random();
+
+    // the default map file for maze.
+    private static String myMazeMap;
 
     // initializing the 2D maze.
     private char[][] myMaze;
 
     //initializing the intersection points
     private ArrayList<Point> myIntersections;
+
+    //initializing the intersection points
+    private ArrayList<Point> myQuestionPoints;
 
     //initialize the scanner
     private Scanner myScanner;
@@ -27,58 +30,79 @@ public class Maze {
      *
      * @throws FileNotFoundException
      */
-    public Maze() throws FileNotFoundException {
+    public Maze(String mapName) throws FileNotFoundException {
         // For Program running purpose.
+        myMazeMap = mapName;
         start();
     }
 
+
     /**
      * This method calls all of the sub-methods that has in this class.
+     *
      * @throws FileNotFoundException
      */
     private void start() throws FileNotFoundException {
-        // read the map file from scanner.
-        myScanner = new Scanner(MAZE_MAP);
-        // Read the Row length from the map.
-        int row = myScanner.nextInt();
-        //// Read the Column length from the map.
-        int column = myScanner.nextInt();
+
         // Assign the instance variable myMaze to the create Maze.
-        myMaze = createMaze(row, column);
+        myMaze = createMaze(myMazeMap);
         // Assign the instance variable myIntersections to the create Maze.
         myIntersections = intersections(myMaze);
-        questionPointgenerator();
+        myQuestionPoints = questionPointgenerator();
     }
 
     /**
      * This method return the 2D maze List.
+     *
      * @return the 2D list.
      */
-    public char[][] getArray(){
+    public char[][] getArray() {
         return myMaze;
     }
 
     // everytime players move just update the map.
-    public void setArray (Point thePoint) {
-        myMaze[(int) thePoint.getX()][(int) thePoint.getY()] = 'M';
+    public void setArray(Point thePoint) {
+        if (myMaze[thePoint.x][thePoint.y] != '@') {
+            Point player = playerLocation();
+            myMaze[player.x][player.y] = '+';
+            myMaze[thePoint.x][thePoint.y] = 'M';
+        }
+
+    }
+
+    private Point playerLocation() {
+        for (int i = 0; i < myMaze.length; i++) {
+            for (int j = 0; j < myMaze.length; j++) {
+                if (myMaze[i][j] == 'M') {
+                    return new Point(i, j);
+                }
+            }
+        }
+        return null;
     }
 
     /**
      * This method reads the character from the map.
      *
-     * @param theRow the row length.
-     * @param theColumn the column length.
-     *
+     * @param theMapName the row length.
      * @return the 2D maze List.
      * @throws FileNotFoundException
      */
-    private char[][] createMaze (int theRow, int theColumn) throws FileNotFoundException{
+    public char[][] createMaze(String theMapName) throws FileNotFoundException {
+
+        File mazeMapFile = new File(theMapName);
+        // read the map file from scanner.
+        myScanner = new Scanner(mazeMapFile);
+        // Read the Row length from the map.
+        int row = myScanner.nextInt();
+        //// Read the Column length from the map.
+        int column = myScanner.nextInt();
         //initialize the array size.
-        char[][] mazeMap = new char[theRow][theColumn];
+        char[][] mazeMap = new char[row][column];
         // read the character from the map.
-        for (int i = 0; i < theRow; i++) {
+        for (int i = 0; i < row; i++) {
             String line = myScanner.next();
-            for (int j = 0; j < theColumn; j++) {
+            for (int j = 0; j < column; j++) {
                 mazeMap[i][j] = line.charAt(j);
             }
         }
@@ -88,11 +112,12 @@ public class Maze {
 
     /**
      * This method return the intersection (x, y)points.
+     *
      * @param theMaze the 2D List.
      * @return the intersections point.
      */
-    private ArrayList<Point> intersections(char[][] theMaze) {
-        ArrayList<Point> intersectionsPoint = new ArrayList <Point>();
+    public ArrayList<Point> intersections(char[][] theMaze) {
+        ArrayList<Point> intersectionsPoint = new ArrayList<Point>();
         for (int i = 0; i < theMaze.length; i++) {
             for (int j = 0; j < theMaze[0].length; j++) {
                 int up = i - 1;
@@ -100,31 +125,49 @@ public class Maze {
                 int left = j - 1;
                 int right = j + 1;
                 // check the road up, down, left, and right, and also the edge cases.
-                if ((up >= 0 && theMaze[up][j] == '+') &&
-                        (down < theMaze.length && theMaze[down][j] == '+') &&
-                        (left >= 0 && theMaze[i][left] == '+') &&
-                        (right < theMaze[0].length && theMaze[i][right] == '+')) {
-                    // add the point into the arrayList.
-                    intersectionsPoint.add(new Point(i, j));
+                if (theMaze[i][j] == '+') {
+                    int counter = 0;
+                    if (up >= 0 && theMaze[up][j] != '@') {
+                        // add the point into the arrayList.
+                        counter++;
+                    }
+                    if (down < theMaze.length && theMaze[down][j] != '@') {
+                        // add the point into the arrayList.
+                        counter++;
+                    }
+                    if (left >= 0 && theMaze[i][left] != '@') {
+                        // add the point into the arrayList.
+                        counter++;
+                    }
+                    if (right < theMaze[0].length && theMaze[i][right] != '@') {
+                        // add the point into the arrayList.
+                        counter++;
+                    }
+                    if (counter > 2) {
+                        intersectionsPoint.add(new Point(j, i));
+                    }
                 }
             }
         }
+        System.out.println(intersectionsPoint);
         return intersectionsPoint;
 
     }
 
 
-    private void questionPointgenerator() {
+    public ArrayList<Point> questionPointgenerator() {
         // initialize how many question will have in the map.
-        int number = myIntersections.size()/2;
+        int number = myIntersections.size() / 2;
+        ArrayList<Point> questionPoint = new ArrayList<Point>();
         for (int i = 0; i < number; i++) {
             //initailize the ranPoints.
             int ranPoints = RANDOM_POINTS.nextInt(myIntersections.size());
             // Get the intersections Point from the ranPoints.
             Point randomIntersection = myIntersections.get(ranPoints);
-            // assign the radom question point in the maze
-            myMaze[randomIntersection.y][randomIntersection.x] = '?';
+            // assign the random question point in the maze
+            questionPoint.add(new Point(randomIntersection.y, randomIntersection.x));
         }
+        return questionPoint;
     }
 
     /**
@@ -140,6 +183,4 @@ public class Maze {
         }
         return sb.toString();
     }
-
 }
-
