c3263e1568e646c65aa54410821ec7253838bc87
ingeun3 <131113666+ingeun3@users.noreply.github.com>
2023-05-18 00:19:25 -0700

    Update Maze.java

--- TriviaMazeGame/src/model/Maze.java
+++ TriviaMazeGame/src/model/Maze.java
@@ -8,24 +8,21 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Maze {
+    // Class constant
 
     // the final Random.
-    private static Random RANDOM_POINTS = new Random();
+    private static final Random RANDOM_POINTS = new Random();
+
+    // Class field
 
     // initializing the 2D maze.
-    private char[][] myMaze;
-
+    private final char[][] myMaze;
+    //initializing the intersection points
+    private final ArrayList<Point> myIntersections;
+    //initializing the intersection points
+    private final ArrayList<Point> myQuestionPoints;
     // initializing the location of the player.
     private Point myPlayerLocation;
-
-    //initializing the intersection points
-    private ArrayList<Point> myIntersections;
-
-    //initializing the intersection points
-    private ArrayList<Point> myQuestionPoints;
-
-    //initialize the scanner
-    private Scanner myScanner;
 
     /**
      * This is a default constructor for Maze class.
@@ -42,7 +39,7 @@ public class Maze {
     }
 
     /**
-     * This method return the 2D maze List.
+     * This method return the 2D array representation of the maze.
      *
      * @return the 2D list.
      */
@@ -50,7 +47,10 @@ public class Maze {
         return myMaze;
     }
 
-    // everytime players move just update the map.
+    /**
+     * Updates the map whenever player makes a move.
+     * @param thePoint The point player wants to move.
+     */
     public void setArray(Point thePoint) {
         if (myMaze[thePoint.y][thePoint.x] != '@') {
             myMaze[myPlayerLocation.y][myPlayerLocation.x] = '+';
@@ -60,6 +60,10 @@ public class Maze {
 
     }
 
+    /**
+     * This method returns the location of the player in Point object.
+     * @return the location of the player.
+     */
     private Point playerLocation() {
         for (int i = 0; i < myMaze.length; i++) {
             for (int j = 0; j < myMaze.length; j++) {
@@ -74,15 +78,16 @@ public class Maze {
     /**
      * This method reads the character from the map.
      *
-     * @param theMapName the row length.
-     * @return the 2D maze List.
-     * @throws FileNotFoundException
+     * @param theMapName the name of the map.
+     * @return the 2D array representation of the maze.
+     * @throws FileNotFoundException when the file is not found.
      */
     public char[][] createMaze(String theMapName) throws FileNotFoundException {
 
         File mazeMapFile = new File(theMapName);
         // read the map file from scanner.
-        myScanner = new Scanner(mazeMapFile);
+        //initialize the scanner
+        Scanner myScanner = new Scanner(mazeMapFile);
         // Read the Row length from the map.
         int row = myScanner.nextInt();
         //// Read the Column length from the map.
@@ -101,10 +106,10 @@ public class Maze {
     }
 
     /**
-     * This method return the intersection (x, y)points.
+     * This method looks for every intersection and return the intersection (x, y)points.
      *
-     * @param theMaze the 2D List.
-     * @return the intersections point.
+     * @param theMaze the 2D array representation of the map.
+     * @return the lists of intersections.
      */
     public ArrayList<Point> intersections(char[][] theMaze) {
         ArrayList<Point> intersectionsPoint = new ArrayList<Point>();
@@ -139,12 +144,14 @@ public class Maze {
                 }
             }
         }
-//        System.out.println(intersectionsPoint);
         return intersectionsPoint;
 
     }
 
-
+    /**
+     * Randomly assigns 50 percent of the intersection points to become a question points.
+     * @return the lists of question points.
+     */
     public ArrayList<Point> questionPointgenerator() {
         // initialize how many question will have in the map.
         int number = myIntersections.size() / 2;
@@ -155,11 +162,18 @@ public class Maze {
             // Get the intersections Point from the ranPoints.
             Point randomIntersection = myIntersections.get(ranPoints);
             // assign the random question point in the maze
-            questionPoint.add(new Point(randomIntersection.y, randomIntersection.x));
+            questionPoint.add(new Point(randomIntersection.x, randomIntersection.y));
         }
         return questionPoint;
     }
 
+    /**
+     *  This method returns the lists of the question points.
+     * @return the list of the question points.
+     */
+    public ArrayList<Point> getQuestionPoints() {
+       return myQuestionPoints;
+    }
     /**
      * For testing purpose.
      */
