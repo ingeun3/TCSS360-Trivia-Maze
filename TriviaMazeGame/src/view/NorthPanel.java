e745df335430f5e44556a173145ae1a1e4fd66df
Kevin Truong <kht5832@gmail.com>
2023-05-21 20:10:53 -0700

    updated main for everyone

--- TriviaMazeGame/src/view/NorthPanel.java
+++ TriviaMazeGame/src/view/NorthPanel.java
@@ -27,7 +27,7 @@ public class NorthPanel extends JPanel{
     // The number of remaining moves display in the right of the NorthPanel.
     private JLabel myMoves;
 
-    GamePanel gp;
+    //GamePanel gp;
 
     /**
      * The default constructor for NorthPanel.
@@ -35,7 +35,7 @@ public class NorthPanel extends JPanel{
      * @param theMoves the number of remaining moves that will display in the right of the NorthPanel.
      */
     public NorthPanel(GamePanel gp, String theLevel, String theMoves) {
-        this.gp = gp;
+        //this.gp = gp;
         myButtonPanel = new JPanel(new FlowLayout());
         myHelpButton = new JButton("Help");
         mySaveButton     = new JButton("Save");
@@ -44,6 +44,8 @@ public class NorthPanel extends JPanel{
         myMoves = new JLabel(theMoves,  SwingConstants.CENTER);
         start();
     }
+
+
 
     /**
      * Initializing NorthPanel.
